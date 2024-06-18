1e3b00ab8404845db6c4150dd3edc86acf98e4a2
tusharkr16 <tusharkumar9871@gmail.com>
2024-06-18 17:26:56 +0530

    initialised app and installed dep

---
+++ src/Components/HeroSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeroSection = () => {
+    return (
+        <div>
+            <div className='bg-primary-color text-center'>
+                <p className='text-tertiary-color p-2'>The health and wellbeing of our patients and health care team will always be our priority, so we follow the best practices for cleanliness </p>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection;
